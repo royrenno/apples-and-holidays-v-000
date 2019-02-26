5ff241f580ca6c461443f3b52849c2dfa83c6ee8
roy renno <royrenno@yahoo.com>
2019-02-26 03:07:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -12,20 +12,20 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << supply
-  
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
-  
+
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].map do |holiday, supplies|
     supplies
   end.flatten
-  
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -36,7 +36,7 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 end
-  
+
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
