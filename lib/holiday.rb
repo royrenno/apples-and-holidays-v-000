5c2c5446feeea7d4dd4140d1a0f0c6435f8d8df8
roy renno <royrenno@yahoo.com>
2019-02-26 02:56:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize}: "
+    puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
     puts"#{holiday.to_s.split("_").map {|w| w.capitalize }.join(" ")}:#{supplies.join(" , ")}"
   end
