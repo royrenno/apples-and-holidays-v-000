5ab412f9e45d159c413d6024f489ed35506f5d02
roy renno <royrenno@yahoo.com>
2019-02-26 02:52:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts" #{holiday.to_s.split("_").map {|w| w.capitalize }.join("")}:#{supplies.join(",")}"
   end
 end
+end
 
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
